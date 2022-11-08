ece3648295cefe483c5dafc0d68c4c0c055a7c64
Darmoroz <darmoroz99@gmail.com>
2022-11-08 11:24:22 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId = null;
+document.body.addEventListener('click', changeBodyColor);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function assignBodyColor() {
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+}
+function changeBodyColor(e) {if (e.target === refs.startBtn) {
+    refs.startBtn.disabled = true;
+    timerId = setInterval(assignBodyColor, 1000);
+  }
+  if (e.target === refs.stopBtn) {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
